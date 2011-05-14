af6d0d532c9060fa94c7efc9c416dfa560d8e90b
ReneDogs <shingen.takeda@gmx.net>
2011-05-14 19:04:24 +0200

    Finished first cut of the User model

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -21,19 +21,12 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
-endrequire 'spec_helper'
 
-describe "LayoutLinks" do
-  describe "GET /layout_links" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get layout_links_index_path
-      response.status.should be(200)
-      
-        it "should have a signup page at '/signup'" do
-    get '/signup'
-    response.should have_selector('title', :content => "Sign up")
-    end
+
+  it "should have a signup page at '/signup'" do
+  get '/signup'
+  response.should have_selector('title', :content => "Sign up")
+  end
   end
   
 
