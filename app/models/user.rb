5d9c1b9c9cc685adb30343484d8be8e7bec6cd5a
ReneDogs <shingen.takeda@gmx.net>
2011-05-21 19:07:03 +0200

    einige Fehler behoben

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,24 +25,22 @@ class User < ActiveRecord::Base
                        :confirmation => true,
                        :length       => { :within => 6..40 }
                        
-                       before_save :encrypt_password
-
-  def has_password?(submitted_password)
-    # Compare encrypted_password with the encrypted version of
-    # submitted_password.
-  end
-  def self.authenticate(email, submitted_password)
-    user = find_by_email(email)
-    return nil  if user.nil?
-    return user if user.has_password?(submitted_password)
-  end
+                       
+  before_save :encrypt_password
+  
+    def has_password?(submitted_password)
+      encrypted_password == encrypt(submitted_password)
+    end
+    
+    def self.authenticate(email, submitted_password)
+       user = find_by_email(email)
+        return nil  if user.nil?
+       return user if user.has_password?(submitted_password)
+     end
+  
   private
 
     def encrypt_password
-      self.encrypted_password = encrypt(password)
-    end
-
-     def encrypt_password
       self.salt = make_salt if new_record?
       self.encrypted_password = encrypt(password)
     end
@@ -58,4 +56,5 @@ class User < ActiveRecord::Base
     def secure_hash(string)
       Digest::SHA2.hexdigest(string)
     end
+    
   end
