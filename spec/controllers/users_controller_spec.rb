a4c1c3acfc8de284b1777caa1b7318bd8bbbbdd8
ReneDogs <shingen.takeda@gmx.net>
2011-05-26 15:09:05 +0200

    Done with sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -85,7 +85,7 @@ describe "GET 'new'" do
           post :create, :user => @attr
         end.should change(User, :count).by(1)
       end
-
+      
       it "should redirect to the user show page" do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
@@ -94,6 +94,11 @@ describe "GET 'new'" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
       end
+      
+      #it "should sign the user in" do
+       # post :create, :user => @attr
+        #controller.should be_signed_in
+      #end
     end
   end
 end
